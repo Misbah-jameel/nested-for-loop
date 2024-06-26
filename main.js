1fc905f3a5332eed88eea7609ce9e57634bdeae2
MisbahDayo <dayomuhammad892@gmail.com>
2024-06-26 23:29:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var n1 = 5;
+var a = "";
+for (var i = 1; i <= n1; i++) {
+    console.log(i);
+    // a += "\n"
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+let n1: number = 5;
+let a: string =""
+
+for(let i: number = 1; i<=n1; i++){
+    console.log(i);
+}
